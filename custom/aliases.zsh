99d2ab5d443d385bd3f2c61a650f507d460ee09e
Manuel Kallenbach <manuel.kallenbach@gmail.com>
2011-05-02 14:52:51 +0200

    bp alias to show urgency with urxvt

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -5,3 +5,4 @@ zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(|.|..) ]] && reply=(
 
 alias wine='WINEARCH=win32 wine'
 alias cuc='cucumber --format CucumberSpinner::ProgressBarFormatter'
+alias bp='echo -e "\a"'
